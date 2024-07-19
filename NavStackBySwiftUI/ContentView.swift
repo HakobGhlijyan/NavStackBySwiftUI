b953c7b066e759244e5d610803c2bd4206aa939c
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-07-19 17:22:51 +0400

    firts part end, start part two

--- NavStackBySwiftUI/ContentView.swift
+++ NavStackBySwiftUI/ContentView.swift
@@ -11,28 +11,89 @@ struct ContentView: View {
     var platforms: [Platform] = [
         .init(name: "Xbox", imageName: "xbox.logo", color: .green),
         .init(name: "Playstation", imageName: "playstation.logo", color: .indigo),
-        .init(name: "Pc", imageName: "pc", color: .pink),
+        .init(name: "PC", imageName: "pc", color: .pink),
         .init(name: "Mobile", imageName: "iphone", color: .mint),
     ]
     
+    var games: [Game] = [
+        .init(name: "Minecraft", rating: "99"),
+        .init(name: "God of War", rating: "98"),
+        .init(name: "Fortnite", rating: "92"),
+        .init(name: "Madden 2023", rating: "88"),
+    ]
+    
     var body: some View {
-        List {
-            Section("Platform") {
-                ForEach(platforms, id: \.name) { platform in
-                    Label(platform.name, systemImage: platform.imageName)
-                        .foregroundStyle(platform.color)
+        NavigationStack {
+            List {
+                Section("Platform") {
+                    ForEach(platforms, id: \.name) { platform in
+                        NavigationLink(value: platform) {
+                            Label(platform.name, systemImage: platform.imageName)
+                                .foregroundStyle(platform.color)
+                        }
+                    }
                 }
+                Section("Games") {
+                    ForEach(games, id: \.name) { game in
+                        NavigationLink(value: game) {
+                            Text(game.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Gaming")
+            .navigationDestination(for: Platform.self) { platform in
+                //0
+                ZStack {
+                    platform.color.ignoresSafeArea()
+                    Label(platform.name, systemImage: platform.imageName)
+                        .font(.largeTitle).bold()
+                }
+//                viewDestination(platform: platform)
+//                ViewDestination(platform: platform)
+            }
+            .navigationDestination(for: Game.self) { game in
+                Text("\(game.name) - \(game.rating)")
+                    .font(.largeTitle).bold()
             }
         }
     }
+    
+    //1 view is func in destination
+    private func viewDestination(platform: Platform) -> some View {
+        ZStack {
+            platform.color.ignoresSafeArea()
+            Label(platform.name, systemImage: platform.imageName)
+                .font(.largeTitle).bold()
+        }
+    }
+    
 }
 
 #Preview {
     ContentView()
 }
 
-struct Platform {
+struct Platform: Hashable {
     let name: String
     let imageName: String
     let color: Color
+}
+
+struct Game: Hashable {
+    let name: String
+    let rating: String
+}
+
+//2 view in destination
+struct ViewDestination: View {
+    var platform: Platform
+    
+    var body: some View {
+        ZStack {
+            platform.color.ignoresSafeArea()
+            Label(platform.name, systemImage: platform.imageName)
+                .font(.largeTitle).bold()
+        }
+    }
 }
