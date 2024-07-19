716fb94bf288b0bb62645ba4d10373a97be746e4
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-07-19 16:50:59 +0400

    Initial Commit

---
+++ NavStackBySwiftUI/NavStackBySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackBySwiftUIApp.swift
+//  NavStackBySwiftUI
+//
+//  Created by Hakob Ghlijyan on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavStackBySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
